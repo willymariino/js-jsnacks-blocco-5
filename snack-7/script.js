71624e53e51debf44ab812a6e19e7949027f4aa7
willymariino <wg.mariino@gmail.com>
2025-03-12 17:30:30 +0100

    snack 7

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,10 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+const result = students.find((element) => {
+  return element.id === 2
+
+})
+
+console.log(result)
