dd0d7c01084a54b6c4192f06737dd383b2f20837
willymariino <wg.mariino@gmail.com>
2025-03-12 14:53:29 +0100

    snack 3 not working

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,13 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+const resultmap = numbers.map(element => {
+    for (i = 0; i < numbers.length; i++) {
+        numbers[i] + 1
+
+    }
+
+    console.log(element)
+})
+
