c2a0cfc42cc09ddb29a546579572988c198f9c35
willymariino <wg.mariino@gmail.com>
2025-03-12 17:16:19 +0100

    snack 3 returns undefined

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,9 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+const evenNumbers = nums.filter =>
+if (numero % 2 === 0) {
+    return true
+}
+return false 
