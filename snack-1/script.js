6e2069105bfb9976acf4b5378b48364f9e343c5e
willymariino <wg.mariino@gmail.com>
2025-03-12 14:24:58 +0100

    completed first snack

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,4 +1,14 @@
 const names = ['Edoardo', 'Simone', 'Francesco'];
 
+names.forEach((element) => {
+
+    console.log(element)
+}
+
+)
+
+
+
+
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
